b8cc7824cf251c8385282ea08c9a3f3edd708799
benny <shenbinsc@qq.com>
2026-05-10 17:03:40 +0800

    trigger deployment

---
+++ docs/assets/main-BBgrgeAX.js
@@ -0,0 +1,1 @@
+import{aG as t}from"./index-DMg6oAiJ.js";const n=t("main",{state:()=>({counter:0,selectedSourceId:null}),getters:{count:({counter:e})=>e},actions:{incrementCounter(e){this.counter+=e}},persist:!0});export{n as u};
