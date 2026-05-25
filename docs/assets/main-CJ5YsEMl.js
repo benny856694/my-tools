b00607db057a915e38f273d4c602b4ab465477e2
Benny Shen <shenbinsc@gmail.com>
2026-05-25 11:02:44 +0800

    deployment

---
+++ docs/assets/main-CJ5YsEMl.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index-CYLGpKNR.js";const n=t("main",{state:()=>({counter:0,selectedSourceId:null}),getters:{count:({counter:e})=>e},actions:{incrementCounter(e){this.counter+=e}},persist:!0});export{n as u};
